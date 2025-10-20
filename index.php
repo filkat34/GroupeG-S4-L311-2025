a50e24a2033be0edea1e2b11d94260dc9124051c
Mathieu LEYSSENE <mathieuleyssene21@gmail.com>
2025-10-20 20:20:32 +0200

    fix: correct include statements and add language attribute to HTML tag

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
-<?php includ 'inc/inc.functions.php'; ?>
+<?php include 'inc/inc.functions.php'; ?>
 <!DOCTYPE HTML>
 <!--
 	Story by HTML5 UP
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 -->
-<html>
+<html lang="en">
 	<head>
 		<title>Story by HTML5 UP</title>
 		<meta charset="utf-8" />
@@ -21,10 +21,10 @@
 						array_key_exist('page', $_GET) ? $_GET['page'] : null
 					); 
 				?>
-				<?php include 'inc/tpls-footer.php'; ?>
+				<?php include 'inc/tpl-footer.php'; ?>
 			</div>
 
-		<?php includes 'inc/inc.js.php'; ?>
+		<?php include 'inc/inc.js.php'; ?>
 
 	</body>
 </html>
