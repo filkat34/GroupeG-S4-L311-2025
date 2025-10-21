87e1e9658b5cc3cf5b7b18cb4e767138bf6383cf
Mathieu LEYSSENE <mathieuleyssene21@gmail.com>
2025-10-21 15:27:41 +0200

    fix: add comments to improve code readability in article.php

--- pages/article.php
+++ pages/article.php
@@ -1,8 +1,10 @@
 <?php
+    // On récupère l'article à afficher selon l'ID
 	$article = getArticleById(
 		array_key_exists('id', $_GET) ? $_GET['id'] : null
 	);
 
+    // Si l'article n'existe pas, on redirige vers l'accueil
 	if(is_null($article) OR !count($article)){
 		header('Location:index.php');
         exit();
@@ -10,6 +12,7 @@
 ?>	
 <section class="banner style1 orient-left content-align-left image-position-right fullscreen onload-image-fade-in onload-content-fade-right">
 	<div class="content">
+        <!-- Affichage du titre et du contenu de l'article -->
 		<h1><?php echo $article['titre'];?></h1>
 		<p class="major"><?php echo $article['texte'];?></p>
 		<ul class="actions stacked">
@@ -17,6 +20,7 @@
 		</ul>
 	</div>
 	<div class="image">
+        <!-- Affichage de l'image de l'article -->
 		<img src="<?php echo $article['image'];?>" alt="" />
 	</div>
 </section>
